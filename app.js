7d003f39eb0dc540766c8e8f161aae6e1974df34
u.yilmaz <unal.yilmaz123@outlook.com>
2021-12-19 13:39:20 +0300

    Patika | Nodejs post detail page added then dynamically redirect to page

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.get('/add_post',(req,res) => {
     res.render('add_post');
 });
 
+app.get('/posts/:id',async(req,res) => {
+    const post = await Post.findById(req.params.id);
+    res.render('post',{post});
+});
+
 app.post('/posts',async(req,res) => {
     await Post.create(req.body);
     res.redirect('/')
